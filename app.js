3e60709c60d1dba4bda1d9c8b5454d94ea27a22d
tamoperationmc-droid <tamoperationmc@gmail.com>
2026-08-21 00:59:54 +0530

    Update back end link

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // ==================== CONFIGURATION ====================
 const API_CONFIG = {
   // Google Apps Script deployment URL - Using /exec endpoint (not /usercache)
-  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbyRQFd7NGwm76-PJinqDk88Ev0dfkO3SlEW8LaQ1QKb4CfxneU_DKv9nHVc35YOOfjP/exec'
+  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbyoFxe90RcM2Z8RqdUY7ut4HMNcecxRZevqXiLifUg-cqcxnRi_nd4c_fuDtQwn73FH/exec'
 };
 
 // Display backend URL on page
