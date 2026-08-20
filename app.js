1268e104ef2dd22eeb91a5f51e3e218123d4c073
tamoperationmc-droid <tamoperationmc@gmail.com>
2026-08-21 00:13:42 +0530

    Bug fixing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // ==================== CONFIGURATION ====================
 const API_CONFIG = {
   // Google Apps Script deployment URL - Using /exec endpoint (not /usercache)
-  BACKEND_URL: 'https://script.google.com/macros/s/AKfycby-FLYyzpbVVRzlSZzLG2k4szGk-5wHXNU07ZxXeIJWJ7DNMm7jAyezfFOk1EqWuppS/exec'
+  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbyRQFd7NGwm76-PJinqDk88Ev0dfkO3SlEW8LaQ1QKb4CfxneU_DKv9nHVc35YOOfjP/exec'
 };
 
 // Display backend URL on page
